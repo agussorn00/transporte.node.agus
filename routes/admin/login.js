916789cdf02d5a73236fa3408518e5df7f346162
Agustin Ornad <agustinornad09@gmail.com>
2022-07-11 04:51:25 -0300

    Login funcional

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
 router.get('/', function(req,res,next) {
     res.render('admin/login',{
         layout:'admin/layout'
     }); 
-})
+});
+
+router.post('/', async function(req,res,next){
+    try{
+        console.log(req.body);
+        var usuario = req.body.usuario;
+        var password = req.body.password;
+
+        var data = await usuariosModel.getUserAndPassword(usuario,password);
+        
+
+        if(data != undefined){
+            res.redirect('/admin/novedades')
+        } else{
+            res.redirect('/admin/novedades')
+        }
+
+    }catch(error){
+        
+    }
+});
 
 module.exports = router;
